6144c69b1178e7f170cd7b33ec5cf07afa9dd9b7
Denys Zhorov <2013dvl@gmail.com>
2019-04-17 13:54:20 +0200

    Creat project and add JS

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let btnStart = document.querySelector('#start'),
+valueBuget = document.querySelector(".budget-value"),
+valueDaybudget = document.querySelector(".daybudget-value"),
+valueLevel = document.querySelector(".level-value"),
+valueExpenses = document.querySelector(".expenses-value"),
+valueOptionalExpenses = document.querySelector(".optionalexpenses-value"),
+valueIncome = document.querySelector(".income-value"),
+valueMonthsavings = document.querySelector(".monthsavings-value"),
+valueYearsavings = document.querySelector(".yearsavings-value"),
+inputItemsExpenses = document.getElementsByClassName('expenses-item'),
+btnToAppeove1 = document.getElementsByTagName("button")[0],
+btnToAppeove2 = document.getElementsByTagName("button")[1],
+btnToCalc = document.getElementsByTagName("button")[2],
+chooseIncome = document.querySelector(".choose-income"),
+savings = document.querySelector("#savings"),
+chooseSum = document.querySelector(".choose-sum"),
+choosePercent = document.querySelector(".choose-percent"),
+yearValue = document.querySelector(".year-value"),
+monthValue = document.querySelector(".month-value"),
+dayValue = document.querySelector(".day-value"),
+inputOptionalExpensesItem = document.querySelectorAll(".optionalexpenses-item");
+
+
+
+console.log(btnStart);
+console.log(valueBuget);
+console.log(valueDaybudget);
+console.log(valueLevel);
+console.log(valueExpenses);
+console.log(valueOptionalExpenses);
+console.log(valueIncome);
+console.log(valueMonthsavings);
+console.log(valueYearsavings);
+console.log(inputItemsExpenses);
+console.log(btnToAppeove1);
+console.log(btnToAppeove2);
+console.log(btnToCalc);
+console.log(inputOptionalExpensesItem);
+console.log(chooseIncome);
+console.log(savings);
+console.log(chooseSum);
+
+console.log(choosePercent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+
